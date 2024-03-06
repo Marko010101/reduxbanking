57a3b3f9c32e55ed18d4e8350d590ac06dc4ca43
Marko <baghashvili.marko@gmail.com>
2024-03-06 23:08:51 +0400

    First project using Redux

---
+++ src/features/customers/customersSlice.js
@@ -0,0 +1,34 @@
+const initialStateCustomer = {
+  fullName: "",
+  natinalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        natinalID: action.payload.natinalID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, natinalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, natinalID, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: "account/updateName", payload: fullName };
+}
